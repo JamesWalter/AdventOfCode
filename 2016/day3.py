75a92965ce99a9c9552fe58fb5b6ac1b00474f78
James Walter <james.walter@grainger.com>
2017-12-02 18:28:36 -0600

    2017 day 2

--- 2016/day3.py
+++ 2016/day3.py
@@ -71,6 +71,6 @@ with open("./2016/day3.txt", "r") as inputfile:
         points = map(long, points)
         triple.append(points)
         if (count + 1) % 3 == 0:
-            possible += reduce(lambda x,y : x+y , map(lambda triangle: 1 if sum(triangle) - max(triangle) > max(triangle) else 0, zip(triple[0], triple[1], triple[2]) ))
+            possible += reduce(lambda x, y: x + y, map(lambda triangle: 1 if sum(triangle) - max(triangle) > max(triangle) else 0, zip(triple[0], triple[1], triple[2])))
             triple = []
 print possible
